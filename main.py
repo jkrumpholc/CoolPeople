6abacfc9c23481b52f4d4502b531a709038471ea
Mundik <jankrumpholc99@gmail.com>
2023-06-29 09:41:06 +0200

    Total cost calculation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 import sys
 from datetime import datetime, timedelta, time
+
 targets = {}
 daytime_minute_cost = 1
 daytime_start = time(hour=8)
@@ -20,7 +21,7 @@ def calculate(csv_string: str):
     cost = 0
     temp_daytime_minute_cost = daytime_minute_cost
     temp_night_minute_cost = night_minute_cost
-    for j in range(math.ceil(delta_time.seconds/60)):
+    for j in range(math.ceil(delta_time.seconds / 60)):
         if j == 5:
             temp_daytime_minute_cost += five_min_bonus
             temp_night_minute_cost += five_min_bonus
@@ -33,9 +34,18 @@ def calculate(csv_string: str):
     targets[number]["total_cost"] += float(cost)
 
 
+def total_cost() -> int:
+    total = 0
+    most_called = list(targets.keys())[list(targets.values()).index(max(targets.values(), key=len))]
+    targets.pop(most_called)
+    for j in targets:
+        total += targets[j]["total_cost"]
+    return total
+
+
 if __name__ == '__main__':
     csv_file = sys.argv[1]
     with open(csv_file) as file:
         for i in file:
             calculate(i.strip())
-    print(targets)
+    print(f"Total cost: {total_cost()} CZK")
